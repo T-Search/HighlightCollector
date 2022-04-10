8dfd117345065a27f53c3177620dff7476b39aba
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-04-10 22:13:03 +0200

    Init spring project

---
+++ src/main/java/de/tsearch/highlightcollector/HighlightCollectorApplication.java
@@ -0,0 +1,13 @@
+package de.tsearch.highlightcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighlightCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighlightCollectorApplication.class, args);
+    }
+
+}
